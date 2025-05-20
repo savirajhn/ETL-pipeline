a990a85957a20ffe10534f3b4b4a820799a0f9c1
savirajhn <163814274+savirajhn@users.noreply.github.com>
2025-05-20 15:09:30 +0700

    Update utils_load.py

--- utils/utils_load.py
+++ utils/utils_load.py
@@ -11,7 +11,7 @@ def save_to_csv(df, filename='products.csv'):
     except Exception as e:
         print(f"[ERROR] Gagal menyimpan ke CSV: {e}")
 
-def save_to_google_sheets(df, json_keyfile='projectetl-460308-e98a6a1bcc21.json', sheet_url='https://docs.google.com/spreadsheets/d/1S4S_eH_7HNQ-Su2evTFauJYBA1KnZoSgL6Ii_yqPym8/edit?usp=sharing'):
+def save_to_google_sheets(df, json_keyfile='projectetl-460308-9a52e562abb8.json', sheet_url='https://docs.google.com/spreadsheets/d/1S4S_eH_7HNQ-Su2evTFauJYBA1KnZoSgL6Ii_yqPym8/edit?usp=sharing'):
     try:
         gc = gspread.service_account(filename=json_keyfile)
         sh = gc.open_by_url(sheet_url)
